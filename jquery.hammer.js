f089e68a8a6e46983c74a95c35a60e215bd5d38d
J. Tangelder <j.tangelder@gmail.com>
2012-05-08 14:49:43 +0200

    jQuery plugin to store Hammer instance in $.data

--- jquery.hammer.js
+++ jquery.hammer.js
@@ -1,6 +1,6 @@
 /*
  * Hammer.JS jQuery plugin
- * version 0.1.1
+ * version 0.2
  * author: Eight Media
  * https://github.com/EightMedia/hammer.js
  */
@@ -8,14 +8,17 @@ jQuery.fn.hammer = function(options)
 {
     return this.each(function()
     {
-        var $el = jQuery(this);
         var hammer = new Hammer(this, options);
+
+        var $el = jQuery(this);
+        $el.data("hammer", hammer);
+
         var events = ['hold','tap','doubletap','transformstart','transform','transformend','dragstart','drag','dragend','release'];
 
         for(var e=0; e<events.length; e++) {
-            hammer['on'+ events[e]] = (function($el, eventName) {
+            hammer['on'+ events[e]] = (function(el, eventName) {
                 return function(ev) {
-                    $el.trigger(jQuery.Event(eventName, ev));
+                    el.trigger(jQuery.Event(eventName, ev));
                 };
             })($el, events[e]);
         }
