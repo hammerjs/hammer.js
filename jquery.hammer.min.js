010eb275fa373e443de18184e418872541af9456
Adrian Maurer <adrian.maurer@gmail.com>
2012-10-31 17:03:27 -0400

    minified

---
+++ jquery.hammer.min.js
@@ -0,0 +1,26 @@
+/*
+ * Hammer.JS jQuery plugin
+ * version 0.3
+ * author: Eight Media
+ * https://github.com/EightMedia/hammer.js
+ */
+jQuery.fn.hammer = function(options)
+{
+    return this.each(function()
+    {
+        var hammer = new Hammer(this, options);
+
+        var $el = jQuery(this);
+        $el.data("hammer", hammer);
+
+        var events = ['hold','tap','doubletap','transformstart','transform','transformend','dragstart','drag','dragend','swipe','release'];
+
+        for(var e=0; e<events.length; e++) {
+            hammer['on'+ events[e]] = (function(el, eventName) {
+                return function(ev) {
+                    el.trigger(jQuery.Event(eventName, ev));
+                };
+            })($el, events[e]);
+        }
+    });
+};
