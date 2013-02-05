4aa5563a138adcc21cb47ad0874154aa3e7012db
J. Tangelder <j.tangelder@gmail.com>
2013-02-05 19:04:08 +0100

    requirejs define

--- dist/hammer-latest.js
+++ dist/hammer-latest.js
@@ -1018,4 +1018,9 @@ Hammer.gestures.Release = {
 // Expose Hammer to the global object
 window.Hammer = Hammer;
 
+// requireJS module definition
+if (typeof window.define === "function" && window.define.amd) {
+	window.define("hammer", [], function() { return Hammer; });
+}
+
 })(window);
--- src/outro.js
+++ src/outro.js
@@ -1,4 +1,9 @@
 // Expose Hammer to the global object
 window.Hammer = Hammer;
 
+// requireJS module definition
+if (typeof window.define === "function" && window.define.amd) {
+	window.define("hammer", [], function() { return Hammer; });
+}
+
 })(window);
