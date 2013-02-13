f73c294d2275193fc6b2f61e797f82d1cf95fc21
J. Tangelder <j.tangelder@gmail.com>
2013-02-13 15:34:29 +0100

    fix carousel ios flicker issue

--- v2/dist/jquery.hammer-1.0.0rc1.js
+++ v2/dist/jquery.hammer-1.0.0rc1.js
@@ -1130,3 +1130,87 @@ if (typeof window.define === 'function' && window.define.amd) {
 }
 
 })(window);
+
+(function($) {
+    /**
+     * bind dom events
+     * this overwrites addEventListener
+     * @param elements
+     * @param types
+     * @param handler
+     */
+    Hammer.event.bindDom = function(element, eventTypes, handler) {
+        $(element).on(eventTypes, function(ev) {
+            var data = ev.originalEvent;
+
+            // IE pageX fix
+            if(!data.pageX) {
+                data.pageX = ev.pageX;
+                data.pageY = ev.pageY;
+            }
+
+            // IE target fix
+            if(!data.target) {
+                data.target = ev.target;
+            }
+
+            // IE button fix
+            if(data.button === 1) {
+                data.which = data.button;
+            }
+
+            handler.call(this, data);
+        });
+    };
+
+    /**
+     * the methods are called by the instance, but with the jquery plugin
+     * we use the jquery event methods instead.
+     * @this Hammer.Instance
+     */
+    Hammer.Instance.prototype.on = function(types, handler) {
+        return $(this.element).on(types, handler);
+    };
+    Hammer.Instance.prototype.off = function(types, handler) {
+        return $(this.element).off(types, handler);
+    };
+
+
+    /**
+     * trigger events
+     * this is called by the gestures to trigger an event like 'tap'
+     * @this Hammer.Instance
+     * @param gesture
+     * @param data
+     */
+    Hammer.Instance.prototype.trigger = function(gesture, eventData){
+        return $(eventData.srcEvent.target).trigger({
+            type: gesture,
+            gesture: eventData
+        });
+    };
+
+
+    /**
+     * jQuery plugin
+     * create instance of Hammer and watch for gestures,
+     * and when called again you can change the options
+     * @param   object      [options={}]
+     * @return  jQuery
+     */
+    $.fn.hammer = function(options) {
+        return this.each(function() {
+            var el = $(this);
+            var inst = el.data("hammer");
+            // start new hammer instance
+            if(!inst) {
+                el.data("hammer", Hammer(this, options || {}));
+            }
+            // change the options
+            else if(inst && options) {
+                Hammer.utils.extend(inst.options, options);
+            }
+        });
+    };
+
+})(jQuery);
