1791cc295824fd4f99cdc4c0ffb11caa9f2d70a9
J. Tangelder <j.tangelder@gmail.com>
2013-02-13 16:10:43 +0100

    carousel with jquery

--- dist/jquery.hammer-1.0.0rc1.js
+++ dist/jquery.hammer-1.0.0rc1.js
@@ -225,7 +225,7 @@ Hammer.event = {
             this.bindDom(element, Hammer.EVENT_TYPES[eventType], function(ev) {
                 // touch must be down or a touch element
                 if(ev.type.match(/down/i) &&
-                    (ev.which === 1 || ev.pointerType == ev.MSPOINTER_TYPE_TOUCH)) {
+                    (ev.which === 1 || (ev.pointerType && ev.pointerType == ev.MSPOINTER_TYPE_TOUCH))) {
                     touchdown = true;
                 }
 
