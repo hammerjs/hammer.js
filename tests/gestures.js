1113d58473a558fa222290bde14cebee815d7ca2
Jorik Tangelder <j.tangelder@gmail.com>
2013-02-24 18:28:54 +0100

    added test for eventData

--- tests/gestures.js
+++ tests/gestures.js
@@ -14,7 +14,7 @@ var all_events = ["touch", "release", "hold", "tap", "doubletap",
 // keep track of what events are triggered
 var triggered_events = {};
 hammertime.on(all_events.join(" "), function(ev) {
-    triggered_events[ev.type] = true;
+    triggered_events[ev.type] = ev;
 });
 
 
@@ -31,14 +31,16 @@ function testGesture(gesture, expect_events, callback) {
     // trigger the gesture faker
     faker.triggerGesture(gesture, function() {
         var expect = expect_events.split(" ");
+        var events = Object.keys(triggered_events);
 
         // trigger callback with true/false is all the events are triggered
         // if also any other events are triggered it is false
-        var success = (Object.keys(triggered_events).length === expect.length);
+        var success = (events.length === expect.length);
 
+        // error msg
         var msg = gesture +" detected";
         if(!success) {
-            msg = gesture +" error. Events thrown: "+ Object.keys(triggered_events).join(" ");
+            msg = gesture +" error. Events thrown: "+ events.join(" ");
         }
         callback(success, msg);
     });
@@ -56,9 +58,8 @@ var gesture_tests = {
     'SwipeRight' : 'touch drag dragstart dragright dragend swipe swiperight release',
     'Rotate' : 'touch transform transformstart transformend rotate release',
     'PinchIn' : 'touch transform transformstart transformend pinch pinchin release',
-    'PinchOut' : 'touch transform transformstart transformend pinch pinchout release',
+    'PinchOut' : 'touch transform transformstart transformend pinch pinchout release'
 };
-
 
 for(var gesture in gesture_tests) {
     if(gesture_tests.hasOwnProperty(gesture)) {
@@ -72,3 +73,52 @@ for(var gesture in gesture_tests) {
         })(gesture);
     }
 }
+
+
+/**
+ * test eventData
+ */
+asyncTest('eventData', function() {
+    faker.triggerGesture('DragRight', function() {
+        var ev = triggered_events['dragright'];
+
+        var checks = [];
+
+        checks.push(ev.type == 'dragright');
+        checks.push(_.isObject(ev.gesture));
+        checks.push(_.isNumber(ev.gesture.angle));
+        checks.push(_.isObject(ev.gesture.center));
+        checks.push(_.isNumber(ev.gesture.deltaTime));
+        checks.push(_.isNumber(ev.gesture.deltaX));
+        checks.push(_.isNumber(ev.gesture.deltaY));
+        checks.push(ev.gesture.direction === Hammer.DIRECTION_RIGHT);
+        checks.push(_.isNumber(ev.gesture.distance));
+        checks.push(ev.gesture.eventType === Hammer.EVENT_MOVE);
+        checks.push(ev.gesture.pointerType === Hammer.POINTER_TOUCH);
+        checks.push(_.isFunction(ev.gesture.preventDefault));
+        checks.push(_.isNumber(ev.gesture.rotation));
+        checks.push(_.isNumber(ev.gesture.scale));
+        checks.push(_.isObject(ev.gesture.srcEvent));
+        checks.push(_.isObject(ev.gesture.startEvent));
+        checks.push(_.isFunction(ev.gesture.stop));
+        checks.push(_.isFunction(ev.gesture.stopPropagation));
+        checks.push(_.isElement(ev.gesture.target));
+        checks.push(_.isNumber(ev.gesture.timestamp));
+        checks.push(_.isArray(ev.gesture.touches));
+        checks.push(_.isNumber(ev.gesture.velocityX));
+        checks.push(_.isNumber(ev.gesture.velocityY));
+
+        var msg = 'EventData tested';
+        var success = true;
+        for(var c=0; c<checks.length; c++) {
+            if(!checks[c]) {
+                success = false;
+                msg = 'EventData Test #'+ c +" failed";
+                break;
+            }
+        }
+
+        ok(success, msg);
+        start();
+    });
+});
