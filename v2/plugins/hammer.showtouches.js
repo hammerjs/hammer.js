f43b937cbac892d20ac46691988e12ce4ebb7214
Jorik Tangelder <j.tangelder@gmail.com>
2013-02-25 18:59:47 +0100

    updated versions

--- v2/plugins/hammer.showtouches.js
+++ v2/plugins/hammer.showtouches.js
@@ -3,8 +3,9 @@
      * ShowTouches gesture
      * requires jQuery
      * show all touch on the screen by placing elements at there pageX and pageY
+     * @param   {Boolean}   [force]
      */
-    Hammer.plugins.showTouches = function() {
+    Hammer.plugins.showTouches = function(force) {
         // the circles under your fingers
         var template = '<div style="position:absolute;z-index:9999;left:0;top:0;height:14px;width:14px;border:solid 2px #777;' +
             'background:rgba(255,255,255,.7);border-radius:20px;pointer-events:none;' +
@@ -27,14 +28,14 @@
             }
         }
 
-        Hammer.gesture.register({
+        Hammer.detection.register({
             name: 'show_touches',
             priority: 0,
             handler: function(ev, inst) {
                 touches_index = {};
 
                 // clear old elements when not using a mouse
-                if(ev.pointerType != Hammer.POINTER_MOUSE) {
+                if(ev.pointerType != Hammer.POINTER_MOUSE && !force) {
                     removeUnusedElements();
                     return;
                 }
@@ -42,6 +43,7 @@
                 // place touches by index
                 for(var t= 0,total_touches=ev.touches.length; t<total_touches;t++) {
                     var touch = ev.touches[t];
+
                     var id = touch.identifier;
                     touches_index[id] = touch;
 
