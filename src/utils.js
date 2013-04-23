897ce969d8a5636f724cf70712ceddaa9ac1527a
Tom Herold <tom.herold@scalableminds.com>
2013-04-23 22:14:30 +0200

    added event delegation

--- src/utils.js
+++ src/utils.js
@@ -195,5 +195,26 @@ Hammer.utils = {
                 return false;
             };
         }
+    },
+
+    matchesSelector: function matchesSelector(element, selector) {
+
+        Element && function(ElementPrototype) {
+            ElementPrototype.matchesSelector = ElementPrototype.matchesSelector ||
+            ElementPrototype.mozMatchesSelector ||
+            ElementPrototype.msMatchesSelector ||
+            ElementPrototype.oMatchesSelector ||
+            ElementPrototype.webkitMatchesSelector ||
+            function (selector) {
+                var node = this, nodes = (node.parentNode || node.document).querySelectorAll(selector), i = -1;
+
+                while (nodes[++i] && nodes[i] != node){}
+
+                return !!nodes[i];
+            };
+        }(Element.prototype);
+
+        return element.matchesSelector(selector);
+
     }
 };
