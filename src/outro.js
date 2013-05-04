8c5d4369cd5ee6f3e339a34255eaf66f9c36fa31
Blaine Bublitz <blaine@iceddev.com>
2013-05-04 11:48:18 -0700

    fix UMD wrapper

--- src/outro.js
+++ src/outro.js
@@ -1,16 +1,21 @@
-// node export
-if(typeof module === 'object' && typeof module.exports === 'object'){
-    module.exports = Hammer;
-}
-// just window export
-else {
+// Based off Lo-Dash's excellent UMD wrapper (slightly modified) - https://github.com/bestiejs/lodash/blob/master/lodash.js#L5515-L5543
+// some AMD build optimizers, like r.js, check for specific condition patterns like the following:
+if(typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
+    // Expose Hammer to the global object even when an AMD loader is present in
+    // case Hammer was injected by a third-party script and not intended to be
+    // loaded as a module.
     window.Hammer = Hammer;
 
-    // requireJS module definition
-    if(typeof window.define === 'function' && window.define.amd) {
-        window.define(function() {
-            return Hammer;
-        });
-    }
+    // define as an anonymous module
+    define(function() {
+        return Hammer;
+    });
+}
+// check for `exports` after `define` in case a build optimizer adds an `exports` object
+else if(typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = Hammer;
+}
+else {
+    window.Hammer = Hammer;
 }
 })(this);
