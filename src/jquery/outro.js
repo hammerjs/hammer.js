6ee7381e01bee91768db46139f7c306ffb7b2f28
stereobit <t@stereob.it>
2013-09-14 18:45:22 +0100

    remove name from define call in jquery outro

--- src/jquery/outro.js
+++ src/jquery/outro.js
@@ -1,5 +1,5 @@
   if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
-    define('hammer-jquery', ['hammer', 'jquery'], init);
+    define(['hammer', 'jquery'], init);
   } else {
     init(window.Hammer, window.jQuery || window.Zepto);
   }
