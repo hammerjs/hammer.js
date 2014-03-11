e1556ebd3a42aff7b9842e9238f89e58c15ad3c3
J. Tangelder <j.tangelder@gmail.com>
2014-03-11 12:11:25 +0100

    separate export from outro

---
+++ src/export.js
@@ -0,0 +1,16 @@
+// AMD export
+if(typeof define === 'function' && define.amd) {
+  define(function(){
+    return Hammer;
+  });
+}
+
+// commonjs export
+else if(typeof module === 'object' && module.exports) {
+  module.exports = Hammer;
+}
+
+// browser export
+else {
+  window.Hammer = Hammer;
+}
