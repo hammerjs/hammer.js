b7c72ed860ece911896010185196053f6f152168
J. Tangelder <j.tangelder@gmail.com>
2014-03-18 13:19:01 +0100

    tracking code

--- examples/assets/js/ga.js
+++ examples/assets/js/ga.js
@@ -5,6 +5,6 @@ _gaq.push(['_trackPageview']);
 
 (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
