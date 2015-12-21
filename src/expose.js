b54a7fd051cbb69a458284a7131345a983e35d26
Chris Thoburn <runspired@gmail.com>
2015-12-21 16:12:35 -0600

    ignore self and appease travis for legacy setup

--- src/expose.js
+++ src/expose.js
@@ -52,7 +52,7 @@ extend(Hammer, {
 
 // this prevents errors when Hammer is loaded in the presence of an AMD
 //  style loader but by script tag, not by the loader.
-var freeGlobal = (typeof window !== 'undefined' ? window : (typeof self !== 'undefined' ? self : {}));
+var freeGlobal = (typeof window !== 'undefined' ? window : (typeof self !== 'undefined' ? self : {})); // jshint ignore:line
 freeGlobal.Hammer = Hammer;
 
 if (typeof define === 'function' && define.amd) {
