085d3a87eab8674c45e9d3a14c4ca44ad7b97e26
unknown <lydia.dumitrescu@Lydia-PC.code40.local>
2016-01-20 18:43:10 +0200

    Manager: Make sure that the required parameters are present
    Closes #908

--- src/manager.js
+++ src/manager.js
@@ -194,6 +194,13 @@ Manager.prototype = {
      * @returns {EventEmitter} this
      */
     on: function(events, handler) {
+        if (events === undefined) {
+            return;
+        }
+        if (handler === undefined) {
+            return;
+        }
+
         var handlers = this.handlers;
         each(splitStr(events), function(event) {
             handlers[event] = handlers[event] || [];
@@ -209,6 +216,10 @@ Manager.prototype = {
      * @returns {EventEmitter} this
      */
     off: function(events, handler) {
+        if (events === undefined) {
+            return;
+        }
+
         var handlers = this.handlers;
         each(splitStr(events), function(event) {
             if (!handler) {
