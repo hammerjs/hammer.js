fdfdddd4ebecbf3e02f400e69d4db585a8702495
Arjun Kathuria <arjun.kathuria8@gmail.com>
2016-05-29 19:05:21 +0530

    exposes global

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-export {Hammer} from './hammer';
+import {Hammer} from './hammer';
+
+// this prevents errors when Hammer is loaded in the presence of an AMD
+//  style loader but by script tag, not by the loader.
+var freeGlobal = (typeof window !== 'undefined' ? window : (typeof self !== 'undefined' ? self : {})); // jshint ignore:line
+freeGlobal.Hammer = Hammer;
+
+if (typeof define === 'function' && define.amd) {
+    define(function() {
+        return Hammer;
+    });
+} else if (typeof module != 'undefined' && module.exports) {
+    module.exports = Hammer;
+} else {
+    window[exportName] = Hammer;
+}
