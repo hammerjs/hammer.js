3d2df61d27461caf7e699e2eacee2931644527eb
Arjun Kathuria <arjun.kathuria8@gmail.com>
2016-06-08 14:54:24 +0530

    chore: removing redundant files

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,11 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-    entry: 'src/testMain.js',
+    entry: 'src/main.js',
     format: 'es6',
     // moduleName: 'HAMMER',
     plugins: [ babel({exclude: 'node_modules/**'}) ],
-    dest: 'tests/testBuild.js',
+    dest: 'hammer.js',
     intro: " (function(window, document, exportName, undefined) { \n'use strict' ",
     outro: "})(window, document, 'Hammer');"
 
