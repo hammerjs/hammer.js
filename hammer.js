57dedf41b195e48bd529401b6db069eb42d35b3f
Henk Kelder <henk.kelder@live.nl>
2019-10-09 17:40:52 +0200

    2.0.9

--- hammer.js
+++ hammer.js
@@ -1,10 +1,26 @@
-/*! Hammer.JS - v2.0.8 - 2016-09-30
+/*! Hammer.JS - v2.0.9 - 2019-10-09
+ * http://hammerjs.github.io/
+ *
+ * Copyright (c)  Jorik Tangelder;
+ * Licensed under the MIT license */
+/*! Hammer.JS - v2.0.9 - 2019-10-09
+ * http://hammerjs.github.io/
+ *
+ * Copyright (c)  Jorik Tangelder;
+ * Licensed under the MIT license */
+/*! Hammer.JS - v2.0.8 - 2019-10-09
+ * http://hammerjs.github.io/
+ *
+ * Copyright (c)  Jorik Tangelder;
+ * Licensed under the MIT license */
+/*! Hammer.JS - v2.0.8 - 2019-10-09
  * http://hammerjs.github.io/
  *
  * Copyright (c)  Jorik Tangelder;
  * Licensed under the MIT license */
 (function(window, document, exportName, undefined) { 
 'use strict';
+
 /**
  * @private
  * use the val2 when val1 is undefined
@@ -21,8 +37,8 @@ var TEST_ELEMENT = document.createElement('div');
 
 var TYPE_FUNCTION = 'function';
 
-var round = Math.round;
-var abs = Math.abs;
+var round = Math.round,
+    abs = Math.abs;
 var now = Date.now;
 
 /**
@@ -213,7 +229,7 @@ function invokeArrayArg(arg, fn, context) {
  * @param {Array} src
  * @param {String} find
  * @param {String} [findByKey]
- * @return {number} -1 when not found, or the index
+ * @return {Boolean|Number} false when not found, or the index
  */
 function inArray(src, find, findByKey) {
   if (src.indexOf && !findByKey) {
@@ -236,119 +252,6 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 } : function (obj) {
   return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
 };
-
-var asyncGenerator = function () {
-  function AwaitValue(value) {
-    this.value = value;
-  }
-
-  function AsyncGenerator(gen) {
-    var front, back;
-
-    function send(key, arg) {
-      return new Promise(function (resolve, reject) {
-        var request = {
-          key: key,
-          arg: arg,
-          resolve: resolve,
-          reject: reject,
-          next: null
-        };
-
-        if (back) {
-          back = back.next = request;
-        } else {
-          front = back = request;
-          resume(key, arg);
-        }
-      });
-    }
-
-    function resume(key, arg) {
-      try {
-        var result = gen[key](arg);
-        var value = result.value;
-
-        if (value instanceof AwaitValue) {
-          Promise.resolve(value.value).then(function (arg) {
-            resume("next", arg);
-          }, function (arg) {
-            resume("throw", arg);
-          });
-        } else {
-          settle(result.done ? "return" : "normal", result.value);
-        }
-      } catch (err) {
-        settle("throw", err);
-      }
-    }
-
-    function settle(type, value) {
-      switch (type) {
-        case "return":
-          front.resolve({
-            value: value,
-            done: true
-          });
-          break;
-
-        case "throw":
-          front.reject(value);
-          break;
-
-        default:
-          front.resolve({
-            value: value,
-            done: false
-          });
-          break;
-      }
-
-      front = front.next;
-
-      if (front) {
-        resume(front.key, front.arg);
-      } else {
-        back = null;
-      }
-    }
-
-    this._invoke = send;
-
-    if (typeof gen.return !== "function") {
-      this.return = undefined;
-    }
-  }
-
-  if (typeof Symbol === "function" && Symbol.asyncIterator) {
-    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
-      return this;
-    };
-  }
-
-  AsyncGenerator.prototype.next = function (arg) {
-    return this._invoke("next", arg);
-  };
-
-  AsyncGenerator.prototype.throw = function (arg) {
-    return this._invoke("throw", arg);
-  };
-
-  AsyncGenerator.prototype.return = function (arg) {
-    return this._invoke("return", arg);
-  };
-
-  return {
-    wrap: function (fn) {
-      return function () {
-        return new AsyncGenerator(fn.apply(this, arguments));
-      };
-    },
-    await: function (value) {
-      return new AwaitValue(value);
-    }
-  };
-}();
 
 var classCallCheck = function (instance, Constructor) {
   if (!(instance instanceof Constructor)) {
@@ -576,7 +479,7 @@ var Recognizer = function () {
 
   createClass(Recognizer, [{
     key: 'set',
-    value: function set(options) {
+    value: function set$$1(options) {
       assign$1(this.options, options);
 
       // also update the touchAction, in case something changed about the directions/enabled state
@@ -846,6 +749,7 @@ var Recognizer = function () {
   return Recognizer;
 }();
 
+
 Recognizer.prototype.defaults = {};
 
 /**
@@ -914,6 +818,7 @@ var AttrRecognizer = function (_Recognizer) {
   return AttrRecognizer;
 }(Recognizer);
 
+
 AttrRecognizer.prototype.defaults = {
   /**
    * @private
@@ -952,6 +857,7 @@ var RotateRecognizer = function (_AttrRecognizer) {
   }]);
   return RotateRecognizer;
 }(AttrRecognizer);
+
 
 RotateRecognizer.prototype.defaults = {
   event: 'rotate',
@@ -997,6 +903,7 @@ var PinchRecognizer = function (_AttrRecognizer) {
   }]);
   return PinchRecognizer;
 }(AttrRecognizer);
+
 
 PinchRecognizer.prototype.defaults = {
   event: 'pinch',
@@ -1109,6 +1016,7 @@ var PanRecognizer = function (_AttrRecognizer) {
   return PanRecognizer;
 }(AttrRecognizer);
 
+
 PanRecognizer.prototype.defaults = {
   event: 'pan',
   threshold: 10,
@@ -1167,6 +1075,7 @@ var SwipeRecognizer = function (_AttrRecognizer) {
   }]);
   return SwipeRecognizer;
 }(AttrRecognizer);
+
 
 SwipeRecognizer.prototype.defaults = {
   event: 'swipe',
@@ -1340,6 +1249,7 @@ var TapRecognizer = function (_Recognizer) {
   return TapRecognizer;
 }(Recognizer);
 
+
 TapRecognizer.prototype.defaults = {
   event: 'tap',
   pointers: 1,
@@ -1427,6 +1337,7 @@ var PressRecognizer = function (_Recognizer) {
   return PressRecognizer;
 }(Recognizer);
 
+
 PressRecognizer.prototype.defaults = {
   event: 'press',
   pointers: 1,
@@ -1507,7 +1418,7 @@ var TouchAction = function () {
 
   createClass(TouchAction, [{
     key: 'set',
-    value: function set(value) {
+    value: function set$$1(value) {
       // find out the touch-action by the event handlers
       if (value === TOUCH_ACTION_COMPUTE) {
         value = this.compute();
@@ -1849,8 +1760,8 @@ function computeInputData(manager, input) {
     session.firstMultiple = false;
   }
 
-  var firstInput = session.firstInput;
-  var firstMultiple = session.firstMultiple;
+  var firstInput = session.firstInput,
+      firstMultiple = session.firstMultiple;
 
   var offsetCenter = firstMultiple ? firstMultiple.center : firstInput.center;
 
@@ -2235,7 +2146,6 @@ var TouchInput = function (_Input) {
   }]);
   return TouchInput;
 }(Input);
-
 function getTouches(ev, type) {
   var allTouches = toArray$1(ev.touches);
   var targetIds = this.targetIds;
@@ -2438,6 +2348,7 @@ var TouchMouseInput = function (_Input) {
   return TouchMouseInput;
 }(Input);
 
+
 function recordTouches(eventType, eventData) {
   if (eventType & INPUT_START) {
     this.primaryTouch = eventData.changedPointers[0].identifier;
@@ -2448,25 +2359,20 @@ function recordTouches(eventType, eventData) {
 }
 
 function setLastTouch(eventData) {
-  var _this2 = this;
-
-  var _eventData$changedPoi = slicedToArray(eventData.changedPointers, 1);
-
-  var touch = _eventData$changedPoi[0];
+  var _eventData$changedPoi = slicedToArray(eventData.changedPointers, 1),
+      touch = _eventData$changedPoi[0];
 
   if (touch.identifier === this.primaryTouch) {
-    (function () {
-      var lastTouch = { x: touch.clientX, y: touch.clientY };
-      _this2.lastTouches.push(lastTouch);
-      var lts = _this2.lastTouches;
-      var removeLastTouch = function removeLastTouch() {
-        var i = lts.indexOf(lastTouch);
-        if (i > -1) {
-          lts.splice(i, 1);
-        }
-      };
-      setTimeout(removeLastTouch, DEDUP_TIMEOUT);
-    })();
+    var lastTouch = { x: touch.clientX, y: touch.clientY };
+    this.lastTouches.push(lastTouch);
+    var lts = this.lastTouches;
+    var removeLastTouch = function removeLastTouch() {
+      var i = lts.indexOf(lastTouch);
+      if (i > -1) {
+        lts.splice(i, 1);
+      }
+    };
+    setTimeout(removeLastTouch, DEDUP_TIMEOUT);
   }
 }
 
@@ -2559,7 +2465,7 @@ var Manager = function () {
 
   createClass(Manager, [{
     key: 'set',
-    value: function set(options) {
+    value: function set$$1(options) {
       assign$1(this.options, options);
 
       // Options that need a little more setup
@@ -2662,7 +2568,7 @@ var Manager = function () {
 
   }, {
     key: 'get',
-    value: function get(recognizer) {
+    value: function get$$1(recognizer) {
       if (recognizer instanceof Recognizer) {
         return recognizer;
       }
@@ -2839,7 +2745,6 @@ var Manager = function () {
   }]);
   return Manager;
 }();
-
 function toggleCssProps(manager, add) {
   var element = manager.element;
 
@@ -2889,7 +2794,6 @@ var Hammer = function Hammer(element, options) {
   options.recognizers = ifUndefined(options.recognizers, Hammer.defaults.preset);
   return new Manager(element, options);
 };
-
 Hammer.VERSION = '2.0.8';
 
 /**
@@ -3075,7 +2979,6 @@ var SingleTouchInput = function (_Input) {
   }]);
   return SingleTouchInput;
 }(Input);
-
 function normalizeSingleTouches(ev, type) {
   var all = toArray$1(ev.touches);
   var changed = toArray$1(ev.changedTouches);
@@ -3226,6 +3129,9 @@ assign$1(Hammer, {
   removeEventListeners: removeEventListeners
 });
 
+var freeGlobal = typeof window !== 'undefined' ? window : typeof self !== 'undefined' ? self : {}; // jshint ignore:line
+freeGlobal.Hammer = Hammer;
+
 /* jshint ignore:start */
 if (typeof define === 'function' && define.amd) {
   define(function () {
@@ -3237,4 +3143,5 @@ if (typeof define === 'function' && define.amd) {
   window[exportName] = Hammer;
 }
 /* jshint ignore:end */
+
 })(window, document, 'Hammer');
